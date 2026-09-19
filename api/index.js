f34fd4c262b6654d070f3ea8302172a7ebefeba4
Gagguverse <gaganisgr8@gmail.com>
2026-09-19 18:15:23 +0530

    fix(vercel): replace createReadStream.pipe with fs.promises.readFile to fix INTERNAL_FUNCTION_INVOCATION_FAILED - Vercel serverless exits when async handler resolves, before stream pipes complete - Now uses await fs.promises.readFile + res.end(data) for full await chain - Added try/catch error boundary in api/index.js - Fixed vercel.json includeFiles to array format for NFT

--- api/index.js
+++ api/index.js
@@ -3,7 +3,21 @@ require('dotenv').config();
 const { handleRequest } = require('../lib/aero-twin-server');
 
 module.exports = async (req, res) => {
-  return handleRequest(req, res);
+  try {
+    await handleRequest(req, res);
+  } catch (err) {
+    console.error('[Vercel Invocation Error]:', err.stack || err);
+    if (!res.headersSent) {
+      res.writeHead(500, {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Access-Control-Allow-Origin': '*'
+      });
+      res.end(JSON.stringify({
+        error: 'internal_function_error',
+        message: err.message
+      }));
+    }
+  }
 };
 
 module.exports.default = module.exports;
